c8e02049d50f9d3539028ee210a3f7df1e088325
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2021-07-27 09:22:51 -0500

    remove functions

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-const functions = require("firebase-functions");
-
-const admin = require("firebase-admin");
-admin.initializeApp();
-
-exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
-  console.log(user);
-});
